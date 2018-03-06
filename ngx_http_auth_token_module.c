2d65d76b17162cb55799d1b0f6fef60f1ac27b50
Galen Palmer <palmergs@gmail.com>
2018-03-05 21:43:17 -0500

    can load configuration from nginx.conf but can't set defaults

--- ngx_http_auth_token_module.c
+++ ngx_http_auth_token_module.c
@@ -6,6 +6,52 @@
 #include <string.h>
 #include "hiredis/hiredis.h"
 
+typedef struct {
+  ngx_str_t redis_host;
+  ngx_int_t redis_port;
+  ngx_str_t cookie_name;
+  ngx_str_t redirect_location;
+} auth_token_main_conf_t;
+
+static ngx_command_t 
+ngx_http_auth_token_commands[] = {
+  {
+    ngx_string("auth_token_redis_host"),
+    NGX_HTTP_MAIN_CONF|NGX_CONF_TAKE1,
+    ngx_conf_set_str_slot,
+    NGX_HTTP_MAIN_CONF_OFFSET,
+    offsetof(auth_token_main_conf_t, redis_host),
+    NULL
+  },
+  {
+    ngx_string("auth_token_redis_port"),
+    NGX_HTTP_MAIN_CONF|NGX_CONF_TAKE1,
+    ngx_conf_set_num_slot,
+    NGX_HTTP_MAIN_CONF_OFFSET,
+    offsetof(auth_token_main_conf_t, redis_port),
+    NULL
+  },
+  {
+    ngx_string("auth_token_cookie_name"),
+    NGX_HTTP_MAIN_CONF|NGX_CONF_TAKE1,
+    ngx_conf_set_str_slot,
+    NGX_HTTP_MAIN_CONF_OFFSET,
+    offsetof(auth_token_main_conf_t, cookie_name),
+    NULL
+  },
+  {
+    ngx_string("auth_token_redirect_location"),
+    NGX_HTTP_MAIN_CONF|NGX_CONF_TAKE1,
+    ngx_conf_set_str_slot,
+    NGX_HTTP_MAIN_CONF_OFFSET,
+    offsetof(auth_token_main_conf_t, redirect_location),
+    NULL
+  },
+
+  ngx_null_command
+};
+
+
 ngx_module_t ngx_http_auth_token_module;
 
 static ngx_int_t
@@ -21,9 +67,9 @@ redirect(ngx_http_request_t *r, ngx_str_t *location)
 }
 
 static ngx_int_t
-lookup_user(ngx_str_t *auth_token, ngx_str_t *user_id)
+lookup_user(auth_token_main_conf_t *conf, ngx_str_t *auth_token, ngx_str_t *user_id)
 {
-  redisContext *context = redisConnect("localhost", 6379);
+  redisContext *context = redisConnect((const char*)conf->redis_host.data, conf->redis_port);
   redisReply *reply = redisCommand(context, "GET %s", auth_token->data);
   if(reply->type == REDIS_REPLY_NIL) {
     return NGX_DECLINED;
@@ -52,20 +98,21 @@ ngx_http_auth_token_handler(ngx_http_request_t *r)
 
   r->main->internal = 1;
 
-  ngx_str_t cookie = (ngx_str_t)ngx_string("auth_token");
-  ngx_str_t location = (ngx_str_t)ngx_string("http://google.com");
-  ngx_int_t lookup;
-  ngx_str_t auth_token;
-  lookup = ngx_http_parse_multi_header_lines(&r->headers_in.cookies, &cookie, &auth_token);
+  auth_token_main_conf_t *conf = ngx_http_get_module_main_conf(r, ngx_http_auth_token_module);
 
-  if(lookup == NGX_DECLINED) {
-    return redirect(r, &location);
+  ngx_int_t cookie_location;
+  ngx_str_t auth_token;
+  cookie_location = ngx_http_parse_multi_header_lines(&r->headers_in.cookies, &conf->cookie_name, &auth_token);
+
+  if(cookie_location == NGX_DECLINED) {
+    return redirect(r, &conf->redirect_location);
   }
 
   ngx_str_t user_id;
-  ngx_int_t lookup_result = lookup_user(&auth_token, &user_id);
+  ngx_int_t lookup_result = lookup_user(conf, &auth_token, &user_id);
+
   if(lookup_result == NGX_DECLINED) {
-    return redirect(r, &location);
+    return redirect(r, &conf->redirect_location);
   }
 
   append_user_id(r, &user_id);
@@ -89,24 +136,44 @@ static ngx_int_t ngx_http_auth_token_init(ngx_conf_t *cf)
   return NGX_OK;
 }
 
+static void*
+ngx_http_auth_token_create_main_conf(ngx_conf_t *cf)
+{
+  auth_token_main_conf_t *conf;
+
+  conf = ngx_pcalloc(cf->pool, sizeof(auth_token_main_conf_t));
+  if (conf == NULL) {
+    return NULL;
+  }
+
+  conf->redis_port = NGX_CONF_UNSET_UINT;
+
+  /* TODO: how to set default values: expansion of ngx_string macro doesn't work
+  conf->redis_port = 6379;
+  conf->redis_host = &ngx_string("localhost");
+  conf->cookie_name = &ngx_string("auth_token");
+  conf->redirect_location = &ngx_string("http://yahoo.com");
+  */
+  return conf;
+}
+
 static ngx_http_module_t
 ngx_http_auth_token_module_ctx = {
-  NULL,                     /* preconfiguration */
-  ngx_http_auth_token_init, /* postconfiguration */
-  NULL,                     /* create main configuration */
-  NULL,                     /* init main configuration */
-  NULL,                     /* create server configuration */
-  NULL,                     /* merge server configuration */
-  NULL,                     /* create location configuration */
-  NULL                      /* merge location configuration */
+  NULL,                                 /* preconfiguration */
+  ngx_http_auth_token_init,             /* postconfiguration */
+  ngx_http_auth_token_create_main_conf, /* create main configuration */
+  NULL,                                 /* init main configuration */
+  NULL,                                 /* create server configuration */
+  NULL,                                 /* merge server configuration */
+  NULL,                                 /* create location configuration */
+  NULL                                  /* merge location configuration */
 };
-
 
 ngx_module_t
 ngx_http_auth_token_module = {
   NGX_MODULE_V1,
   &ngx_http_auth_token_module_ctx,  /* module context */
-  NULL,                             /* module directives */
+  ngx_http_auth_token_commands,     /* module directives */
   NGX_HTTP_MODULE,                  /* module type */
   NULL,                             /* init master */
   NULL,                             /* init module */
--- spec/integration/integration_spec.rb
+++ spec/integration/integration_spec.rb
@@ -1,9 +1,33 @@
 require "spec_helper"
 
 describe "Integration Specs" do
-  describe "Bootstrap" do
-    it "Webserver is running" do
-      expect(Curl.get("http://127.0.0.1:8888").response_code).to eq(200)
+
+  before do 
+    @redis = Redis.new
+    @redis.flushdb
+  end
+
+  after do
+    @redis.flushdb
+  end
+
+  describe "Use cases" do
+    it "Redirects when no auth token is present" do
+      http = Curl.get("http://127.0.0.1:8888") do |http|
+        http.headers["Cookie"] = ""
+      end
+
+      expect(http.response_code).to eq(302)
+    end
+
+    it "Allows the request when the auth token is valid" do
+      @redis.set("test", "success")
+
+      http = Curl.get("http://127.0.0.1:8888") do |http|
+        http.headers["Cookie"] = "auth_token=test"
+      end
+
+      expect(http.response_code).to eq(200)
     end
   end
 end
